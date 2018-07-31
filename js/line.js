392fcef9324623fde871882ba50f2ef0e356eff8
ma-za-kpe <makpalyy@gmail.com>
2018-07-31 15:49:56 +0300

    add font size 10 to the xaxis labels

--- js/line.js
+++ js/line.js
@@ -1,14 +1,14 @@
 $(function(){
   $.ajax({
-    url: 'http://localhost/dash/column1.php',
+    url: 'http://localhost/dash/line.php',
     type: 'GET',
     success : function(data) {
       chartData = data;
       var chartProperties = {
-        "caption": "Level of tuition fee across all partner schools",
+        "caption": "Size of schools based on student numbers",
         "xAxisName": "School",
-        "yAxisName": "LEVEL OF TUTION (UGX)",
-        "yAxisMaxValue": "4000000",
+        "yAxisName": "Nmmber of students",
+        "yAxisMaxValue": "800",
         "yAxisMinValue": "0",
         "numDivLines" : "8",
         "rotatevalues": "1",
@@ -16,63 +16,19 @@ $(function(){
         "bgColor": "#ffffff",
         "placeLabelsInside": "1",
         "showValues": "0",
-
+        "plotSpacePercent":"320",
+        "baseFontSize": "10"
       };
       apiChart = new FusionCharts({
-        type: 'bar2d',
+        type: 'column2d',
         renderAt: '2chart-container',
-        width: '750',
+        width: '850',
         height: '350',
         dataFormat: 'json',
         dataSource: {
           "chart": chartProperties,
 
-      "data": [
-               {
-                   "label": "Iganga Parents Secondary School",
-                   "value": "3549000"
-               },
-               {
-                   "label": "Bukooli College, Bugiri",
-                   "value": "3102400"
-               },
-               {
-                   "label": "Green Hill Academy School",
-                   "value": "1600000"
-               },
-               {
-                   "label": "Taibah International School",
-                   "value": "1600000"
-               },
-               {
-                   "label": "St Mary.s College Kisubi",
-                   "value": "900000"
-               },
-               {
-                   "label": "Kawempe Muslim Secondary School",
-                   "value": "800000"
-               },
-               {
-                   "label": "Our Lady of Africa Secondary School, Namilyango",
-                   "value": "790000"
-               },
-               {
-                   "label": "Wanyange Girls Secondary School",
-                   "value": "780000"
-               },
-               {
-                   "label": "Uganda Martrys High School, Rubaga",
-                   "value": "760000"
-               }],
-               "trendlines": [{
-          "line": [{
-            "startvalue": "790000",
-            "color": "#032d4f",
-            "thickness": "4",
-            "valueOnRight": "1",
-            "displayvalue": "790000"
-          }]
-        }]
+      "data": chartData
 
          }
    }).render();
