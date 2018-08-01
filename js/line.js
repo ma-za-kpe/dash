58d7a427f17d6609a118f5a02f62f8dac1bd2405
ma-za-kpe <makpalyy@gmail.com>
2018-08-01 08:37:20 +0300

    add data to the line chart

--- js/line.js
+++ js/line.js
@@ -5,24 +5,18 @@ $(function(){
     success : function(data) {
       chartData = data;
       var chartProperties = {
-        "caption": "Number of schools enrolled",
+        "caption": "Number of schools enrolled over time, with a projection for the next 12 months",
         "xAxisName": "Time(months)",
-        "yAxisName": "Nmmber of students",
-        "yAxisMaxValue": "800",
+        "yAxisName": "Number of schools enrolled",
+        "yAxisMaxValue": "40",
         "yAxisMinValue": "0",
         "numDivLines" : "8",
-        "rotatevalues": "1",
-        "theme": "zune",
-        "bgColor": "#ffffff",
-        "placeLabelsInside": "1",
         "showValues": "0",
-        "plotSpacePercent":"320",
-        "baseFontSize": "10",
-        "showvalues": "0",
+        "lineThickness": "3",
+        "yAxisNameFontSize": "15",
+        "legendItemFontSize": "12",
         "anchorRadius": "4",
         "anchorBorderThickness": "2",
-        "theme" : "fint"
-        "lineColor" : "#222222"
       };
       apiChart = new FusionCharts({
         type: 'line',
@@ -33,7 +27,48 @@ $(function(){
         dataSource: {
           "chart": chartProperties,
 
-      "data": chartData
+      "data": [
+               {
+                   "label": "23-11-2009",
+                   "value": "31"
+               },
+               {
+                   "label": "9-4-10",
+                   "value": "33"
+               },
+               {
+                   "label": "9-10-2011",
+                   "value": "28"
+               },
+               {
+                   "label": "24-10-2012",
+                   "value": "20"
+               },
+               {
+                   "label": "11-9-2013",
+                   "value": "27"
+               },
+               {
+                   "label": "11-12-14",
+                   "value": "27"
+               },
+               {
+                   "label": "1-5-2015",
+                   "value": "24"
+               },
+               {
+                   "label": "15-11-2016",
+                   "value": "35"
+               },
+               {
+                   "label": "17-09-201",
+                   "value": "32"
+               },{
+                   "label": "4-9-2018",
+                   "color":"#840f0d",
+                   "value": "40"
+               }
+             ]
 
          }
    }).render();
